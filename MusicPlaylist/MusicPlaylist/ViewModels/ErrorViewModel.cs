90ec14362ef29057e5297dfd691e67ea9a74dda6
Veronika06278 <veronika.zheleva2006@gmail.com>
2024-06-15 19:33:33 +0300

    Add new artist feature completed. ErrorViewModel restored

---
+++ MusicPlaylist/MusicPlaylist/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MusicPlaylist.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
